fcb33578d8018de84f6b2eb05e59abd2c8081578
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-08-10 07:54:57 +0200

    Use templates to replace by data.

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const pageOverview = fs.readFileSync(`${__dirname}/page/overview.html`, "utf-8");
+const pageCard = fs.readFileSync(`${__dirname}/page/overview.html`, "utf-8");
+const pageProduct = fs.readFileSync(`${__dirname}/page/overview.html`, "utf-8");
+
 const data = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
 
+	//Page overview
 	if (pathName === "/overview" || pathName === "/") {
-		res.end("Hello from overview");
+		res.writeHead(200, { "Content-type": "text/html" });
+		res.end(pageOverview);
+
+		//Page product
 	} else if (pathName === "/product") {
 		res.end("Hello from product");
+
+		//Api
 	} else if (pathName === "/api") {
 		res.writeHead(200, { "Content-type": "application/json" });
 		res.end(data);
+
+		//Not found
 	} else {
 		res.writeHead(404);
 		res.end("Page no found!");
