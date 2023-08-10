3b8eae482742ca12cd7b6f994785354e3259e075
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-08-10 08:48:46 +0200

    Put function reaplaceTemplate into modules and export it to index.js

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
-const replaceTemplate = (template, product) => {
-	let output = template.replace(/{%ID%}/g, product.id);
-	output = output.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-	if (!product.organic) {
-		output = output.replace("{%NOT_ORGANIC%}", "not-organic");
-	}
-
-	return output;
-};
+const {replaceTemplate} = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(`${__dirname}/page/overview.html`, "utf-8");
 const tempProduct = fs.readFileSync(`${__dirname}/page/product.html`, "utf-8");
